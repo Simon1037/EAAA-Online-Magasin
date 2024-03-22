fbf0029a46c873917750b20e615cf0edbb013f52
Simon1037 <nomis1037@gmail.com>
2024-03-22 02:33:05 +0100

    Allmost done with the article
    * Reworked a little of the folder structure
    * Reworked the metadata in the head tag
    
    + Added actual images provited by me.
    + Added favicon
    + Added font: "Figtree"
    + Added default font sizes
    + Added / Fixed the .enlarge_ing event
    
    - Removed the Accent that was not really in use.
    
    // Todo: Work on the metadata
    // Todo: Add a landing page
    // Todo: Make the index file the landing page
    // Todo: Make all the links work or make them explain why they dont work with a modal (popup).
    // Todo: Finish the Article
    
    And more

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 /* Adding an accordion */
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+window.onload = function () {
+  var acc = document.getElementsByClassName("accordion");
+  for (var i = 0; i < acc.length; i++) {
+    // Add the 'active' class to each accordion button
+    acc[i].classList.add("active");
+
+    // Immediately expand the panel on window load
+    var panel = acc[i].nextElementSibling;
+    panel.style.maxHeight = panel.scrollHeight + "px";
+
+    // Add click event listener to toggle panel open/close
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+};
+
+/* Adding the ability for the class .enlarge_img to enlarge the image when clicked */
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Select all elements with the .enlarge_img class
+  const enlargeImgs = document.querySelectorAll(".enlarge_img");
+
+  // Add a click event listener to each .enlarge_img element
+  enlargeImgs.forEach((enlargeImg) => {
+    enlargeImg.addEventListener("click", function (e) {
+      e.stopPropagation(); // Stop the click from propagating
+      // Toggle the 'clicked' class for the clicked .enlarge_img
+      this.classList.toggle("clicked");
+    });
   });
-}
+
+  // Listen for clicks on the document to remove the 'clicked' class from all .enlarge_img elements
+  document.addEventListener("click", function (e) {
+    enlargeImgs.forEach((enlargeImg) => {
+      if (!enlargeImg.contains(e.target)) {
+        enlargeImg.classList.remove("clicked");
+      }
+    });
+  });
+});
